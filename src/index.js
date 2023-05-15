5295698f8825c556bc96fa2dc48d59c144251d77
Louis Klein <louisklein71@gmail.com>
2023-05-14 21:10:14 -0700

    tested in js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 console.log(Math.floor(Math.random()*100))
 //Math.floor(Math.random()*dadJokeArray.length)
+
+// const dadJoke = document.querySelector('#dad-joke');
+// const jokeButton = document.querySelector('#joke-button')
+
+// function getJokeData(){
+//     return fetch('https://icanhazdadjoke.com/').then(r => r.json())
+// }
+
+// function displayJoke(jokeData){
+//     dadJoke.textContent = jokeData.joke;
+// }
+
+// jokeButton.addEventListener('click', function(){getJokeData().then(data => displayJoke(data))})
+
+
+// fetch('https://icanhazdadjoke.com/') will return random joke without needing randomizer, would probably cause issues down the line
+// fetch('https://icanhazdadjoke.com/j/<JOKEID>') would return joke for specific id
+// joke ids completely random, 744 total jokes
+// create some sort of loop that fetches entire db? 
+// will need entire db to add to object "likes" and "dislikes"
