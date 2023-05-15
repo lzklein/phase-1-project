6f1cbf746b244a63bb2838f099d4bbf73c6ed9c0
Louis Klein <louisklein71@gmail.com>
2023-05-15 08:46:18 -0700

    added db

--- src/index.js
+++ src/index.js
@@ -15,8 +15,6 @@ console.log(Math.floor(Math.random()*100))
 // jokeButton.addEventListener('click', function(){getJokeData().then(data => displayJoke(data))})
 
 
-// fetch('https://icanhazdadjoke.com/') will return random joke without needing randomizer, would probably cause issues down the line
-// fetch('https://icanhazdadjoke.com/j/<JOKEID>') would return joke for specific id
-// joke ids completely random, 744 total jokes
-// create some sort of loop that fetches entire db? 
-// will need entire db to add to object "likes" and "dislikes"
+//for just generating jokes just fetch and display joke no need to persist
+//after like or dislike, then save a copy of joke object with like/dislike 
+//then post/patch copy to own db
