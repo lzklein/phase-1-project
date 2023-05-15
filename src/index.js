dc146215bbb9e83e20e4ce228a3d389c052722e3
Louis Klein <louisklein71@gmail.com>
2023-05-15 15:36:10 -0700

    pull my finger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 const dadJoke = document.querySelector('#dad-joke');
 const jokeButton = document.querySelector('#joke-button');
 const accordion = document.querySelectorAll('.accordion');
+const jokeWindow = document.querySelector('.joke-window');
+const finger = document.querySelector('#pull-my-finger');
 
 // get the data from the api
 function getJokeData(){
@@ -27,13 +29,19 @@ function likeDislike(){
     dislikeButton.classList='like-dislike';
     likeButton.textContent = '😂';
     dislikeButton.textContent = '😐';
-    // add event listeners to like and dislike that save and change like values
-
+// add event listeners to like and dislike that save and change like values
+    likeButton.addEventListener('click', function(){
+        console.log("I like this")
+        console.log(dadJoke.textContent[0])
+    })
+    dislikeButton.addEventListener('click', function(){
+        console.log("please stop")
+        console.log(dadJoke.textContent[0])
+    })
     dadJoke.append(likeButton, dislikeButton);
 }
 
 // populate top and bottom 5 with jokes based on number of likes
-
 
 //make top and bottom 5 collapsible click is temp change to mouseover to toggle on and mouseleave to toggle off for final
 accordion.forEach(button => button.addEventListener('mouseover', function(){
@@ -46,6 +54,13 @@ accordion.forEach(button => button.addEventListener('mouseleave', function(){
     content.style.display = "none";
 }))
 
+const fart = document.createElement('audio');
+fart.src='fart.mp3';
+
+finger.addEventListener('click', function(){
+    fart.play();
+})
+
 // have the joke button display a new joke
 jokeButton.addEventListener('click', function(){getJokeData()
     .then(function(joke){
@@ -55,14 +70,15 @@ jokeButton.addEventListener('click', function(){getJokeData()
 })
 
 
-//add to joke button eventlistener after display joke, create the like and dislike buttons and append to the joke
-//
-
 //for just generating jokes just fetch and display joke no need to persist
 //after like or dislike, then save a copy of joke object with like/dislike 
 //then post/patch copy to own db
+
+
 
 //PROPOSED CHANGE TO JOKE BUTTON:
 //button text: can I get a dad joke?
 //text appears: I don't know, caaaan you?
 //button changes: may I get a dad joke?
+
+//other stretch goal: have submit box only at start, after user types something in, title of "hi (whatever user submitted) I'm dad and rest of site fades in"
